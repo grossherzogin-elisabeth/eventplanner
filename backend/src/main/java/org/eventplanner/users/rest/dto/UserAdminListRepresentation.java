32ef3ccc694cc4ee3c952f59a2027f2a663b133b
Malte Schwitters <malte.schwitters@outlook.de>
2024-10-22 08:19:38 +0200

    add missing nickname to admin representation

--- backend/src/main/java/org/eventplanner/users/rest/dto/UserAdminListRepresentation.java
+++ backend/src/main/java/org/eventplanner/users/rest/dto/UserAdminListRepresentation.java
@@ -5,6 +5,7 @@ import org.eventplanner.users.entities.UserDetails;
 import org.eventplanner.users.entities.UserQualification;
 import org.eventplanner.users.values.Role;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
@@ -14,29 +15,23 @@ import java.util.Objects;
 public record UserAdminListRepresentation(
     @NonNull String key,
     @NonNull String firstName,
+    @Nullable String nickName,
     @NonNull String lastName,
     @NonNull List<String> positions,
     @NonNull List<String> roles,
-    long expiredQualificationCount,
-    long soonExpiringQualificationCount
+    @NonNull List<UserQualificationRepresentation> qualifications
 ) implements Serializable {
     public static UserAdminListRepresentation fromDomain(@NonNull UserDetails user) {
         return new UserAdminListRepresentation(
             user.getKey().value(),
             user.getFirstName(),
+            user.getNickName(),
             user.getLastName(),
             user.getPositions().stream().map(PositionKey::value).toList(),
             user.getRoles().stream().map(Role::value).toList(),
             user.getQualifications().stream()
-                .map(UserQualification::getExpiresAt)
-                .filter(Objects::nonNull)
-                .filter(d -> d.isBefore(ZonedDateTime.now()))
-                .count(),
-            user.getQualifications().stream()
-                .map(UserQualification::getExpiresAt)
-                .filter(Objects::nonNull)
-                .filter(d -> d.isBefore(ZonedDateTime.now().plusMonths(3)))
-                .count()
+                .map(UserQualificationRepresentation::fromDomain)
+                .toList()
         );
     }
 }
