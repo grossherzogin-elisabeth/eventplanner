bdfbc243dc40d1eb3e73064c20cd36873bc71543
Malte Schwitters <malte.schwitters@outlook.de>
2024-11-03 08:53:24 +0100

    add dismissable prop

--- frontend/src/ui/components/common/alerts/VInfo.vue
+++ frontend/src/ui/components/common/alerts/VInfo.vue
@@ -1,16 +1,24 @@
 <template>
-    <div class="flex items-center rounded-xl bg-blue-200 p-2 pl-4 text-blue-900" :class="{ hidden: dismissed }">
+    <div class="flex items-center rounded-xl bg-blue-100 p-2 pl-4 text-blue-900" :class="{ hidden: dismissed }">
         <i class="fa-solid fa-info-circle"></i>
         <span class="ml-4 mr-1 w-0 flex-grow">
             <slot></slot>
         </span>
-        <button class="h-9 w-9 self-baseline rounded-full hover:bg-yellow-200 hover:bg-opacity-75" @click="dismiss()">
+        <button
+            v-if="props.dismissable === true"
+            class="h-9 w-9 self-baseline rounded-full hover:bg-blue-300 hover:bg-opacity-75"
+            @click="dismiss()"
+        >
             <i class="fa-solid fa-xmark"></i>
         </button>
     </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
+
+interface Props {
+    dismissable?: boolean;
+}
 
 type Emits = (e: 'dismiss') => void;
 
@@ -19,6 +27,7 @@ type Emits = (e: 'dismiss') => void;
  * Component Definition
  * --------------------------------------------------------------------------------------------------------
  */
+const props = defineProps<Props>();
 const emit = defineEmits<Emits>();
 
 const dismissed = ref<boolean>(false);
--- frontend/src/ui/components/common/alerts/VWarning.vue
+++ frontend/src/ui/components/common/alerts/VWarning.vue
@@ -4,13 +4,21 @@
         <span class="ml-4 mr-1 w-0 flex-grow">
             <slot></slot>
         </span>
-        <button class="h-9 w-9 rounded-full hover:bg-yellow-200 hover:bg-opacity-75" @click="dismiss()">
+        <button
+            v-if="props.dismissable === true"
+            class="h-9 w-9 rounded-full hover:bg-yellow-200 hover:bg-opacity-75"
+            @click="dismiss()"
+        >
             <i class="fa-solid fa-xmark"></i>
         </button>
     </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
+
+interface Props {
+    dismissable?: boolean;
+}
 
 type Emits = (e: 'dismiss') => void;
 
@@ -19,6 +27,7 @@ type Emits = (e: 'dismiss') => void;
  * Component Definition
  * --------------------------------------------------------------------------------------------------------
  */
+const props = defineProps<Props>();
 const emit = defineEmits<Emits>();
 
 const dismissed = ref<boolean>(false);
