b3c1c4e1ed43287606210c489f506f005c158c71
Malte Schwitters <malte.schwitters@outlook.de>
2024-11-14 12:44:45 +0100

    remove outdate deployment files

--- terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  stack = "eventplanner-${var.env}"
-
-  default_tags = {
-    environment = var.env
-    app         = "eventplanner"
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "azurerm_container_app_url" {
-  value = azurerm_container_app.eventplanner.latest_revision_fqdn
-}
