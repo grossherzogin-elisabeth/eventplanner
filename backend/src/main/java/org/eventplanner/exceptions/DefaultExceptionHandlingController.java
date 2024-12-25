857ad0921cac90dae0545059ea2082fd54543e9f
Malte Schwitters <malte.schwitters@outlook.de>
2024-12-25 12:42:55 +0100

    add exception handling controller

---
+++ backend/src/main/java/org/eventplanner/exceptions/DefaultExceptionHandlingController.java
@@ -0,0 +1,27 @@
+package org.eventplanner.exceptions;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DefaultExceptionHandlingController {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Void> handleNoSuchElementException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @ExceptionHandler(MissingPermissionException.class)
+    public ResponseEntity<Void> handleMissingPermissionException() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Void> handleUnauthorizedException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
