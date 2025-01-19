7f053e7098b2467f2861e85d7a8cd85d6a8d65fb
Malte Schwitters <malte.schwitters@outlook.de>
2025-01-20 00:08:53 +0100

    log unexpected exceptions

--- backend/src/main/java/org/eventplanner/exceptions/DefaultExceptionHandlingController.java
+++ backend/src/main/java/org/eventplanner/exceptions/DefaultExceptionHandlingController.java
@@ -7,8 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class DefaultExceptionHandlingController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleException(Exception e) {
+        log.error("Unexpected exception", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Void> handleNoSuchElementException() {
