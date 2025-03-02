648a7ed959bd96dfbc5da39ac8e5c0468f381bf6
Malte Schwitters <malte.schwitters@outlook.de>
2025-03-02 12:34:04 +0100

    squash migration

--- backend/src/main/resources/db/migration/V3__init_qualifications.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE qualifications
-(
-    key         TEXT PRIMARY KEY,
-    name        TEXT    NOT NULL,
-    icon        TEXT    NOT NULL,
-    description TEXT    NOT NULL,
-    expires     INTEGER NOT NULL
-);
