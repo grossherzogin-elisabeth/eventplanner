5694d22f55d49f511304aee682bba0ff058867a4
Malte Schwitters <malte.schwitters@outlook.de>
2025-04-09 21:20:02 +0200

    improve exception logging

--- backend/src/main/java/org/eventplanner/events/rest/GlobalExceptionHandlingController.java
+++ backend/src/main/java/org/eventplanner/events/rest/GlobalExceptionHandlingController.java
@@ -1,5 +1,6 @@
 package org.eventplanner.events.rest;
 
+import java.io.IOException;
 import java.util.NoSuchElementException;
 
 import org.eventplanner.events.domain.exceptions.MissingPermissionException;
@@ -18,17 +19,30 @@ public class GlobalExceptionHandlingController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleException(Exception e, HttpServletRequest request) {
-        if (request != null) {
-            log.error("Unhandled exception on request {} {}", request.getMethod(), request.getRequestURI(), e);
-        } else {
-            log.error("An unexpected exception occurred", e);
+        if (isBrokenPipe(e)) {
+            log.info(
+                "A broken pipe exception occurred on request {} {}",
+                request.getMethod(),
+                request.getRequestURI()
+            );
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+        log.error("Unhandled exception on request {} {}", request.getMethod(), request.getRequestURI(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
+    private boolean isBrokenPipe(Throwable e) {
+        return (e instanceof IOException && e.getMessage().contains("Broken pipe"))
+            || isBrokenPipe(e.getCause());
+    }
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Void> handleNoSuchElementException() {
-        log.warn("Tried to access non-existent element");
+    public ResponseEntity<Void> handleNoSuchElementException(NoSuchElementException e, HttpServletRequest request) {
+        log.error(
+            "Tried to access non existing element on request {} {}",
+            request.getMethod(),
+            request.getRequestURI()
+        );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
