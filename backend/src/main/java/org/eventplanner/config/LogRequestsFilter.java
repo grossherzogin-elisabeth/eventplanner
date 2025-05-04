98ef522a2878f4a6aeb2fda76030e4b7ca509f54
Malte Schwitters <malte.schwitters@outlook.de>
2025-05-04 12:59:21 +0200

    improve request logging

--- backend/src/main/java/org/eventplanner/config/LogRequestsFilter.java
+++ backend/src/main/java/org/eventplanner/config/LogRequestsFilter.java
@@ -22,10 +22,40 @@ public class LogRequestsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
         @NonNull final HttpServletRequest request,
         @NonNull final HttpServletResponse response,
-        final FilterChain filterChain
+        @NonNull final FilterChain filterChain
     )
     throws ServletException, IOException {
-        log.debug("{} {}", request.getMethod(), request.getRequestURI());
+        if (request.getMethod().equals("OPTIONS") || isFrontendRequest(request)) {
+            log.trace("Received request: {} {}", request.getMethod(), request.getRequestURI());
+        } else if (isBackendRequest(request)) {
+            log.debug("Received request: {} {}", request.getMethod(), request.getRequestURI());
+        } else {
+            // unexpected requests, might be potential attacks
+            log.info("Received unexpected request: {} {}", request.getMethod(), request.getRequestURI());
+        }
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isBackendRequest(@NonNull final HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/api");
+    }
+
+    private boolean isFrontendRequest(@NonNull final HttpServletRequest request) {
+        if (!request.getMethod().equals("GET")) {
+            return false;
+        }
+        return request.getRequestURI().equals("/")
+            || request.getRequestURI().equals("/favicon.svg")
+            || request.getRequestURI().equals("/registerSW.js")
+            || request.getRequestURI().startsWith("/assets/")
+            || request.getRequestURI().startsWith("/auth")
+            || request.getRequestURI().startsWith("/login")
+            || request.getRequestURI().startsWith("/events")
+            || request.getRequestURI().startsWith("/users")
+            || request.getRequestURI().startsWith("/settings")
+            || request.getRequestURI().startsWith("/info")
+            || request.getRequestURI().startsWith("/account")
+            || request.getRequestURI().startsWith("/wiki")
+            || request.getRequestURI().startsWith("/basedata");
     }
 }
