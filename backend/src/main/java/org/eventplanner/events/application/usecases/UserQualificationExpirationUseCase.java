e84ce8fb3ec7a57114a5c19fe01a29caff06fbbd
Malte Schwitters <malte.schwitters@outlook.de>
2025-06-15 14:32:45 +0200

    read notification enabled toggle on cron job

--- backend/src/main/java/org/eventplanner/events/application/usecases/UserQualificationExpirationUseCase.java
+++ backend/src/main/java/org/eventplanner/events/application/usecases/UserQualificationExpirationUseCase.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import org.eventplanner.events.application.ports.PositionRepository;
 import org.eventplanner.events.application.ports.QualificationRepository;
+import org.eventplanner.events.application.services.ConfigurationService;
 import org.eventplanner.events.application.services.NotificationService;
 import org.eventplanner.events.application.services.UserService;
 import org.eventplanner.events.domain.entities.users.UserQualification;
+import org.eventplanner.events.domain.values.notifications.NotificationType;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +20,17 @@ import lombok.extern.slf4j.Slf4j;
 public class UserQualificationExpirationUseCase {
 
     private final UserService userService;
+    private final ConfigurationService configurationService;
     private final NotificationService notificationService;
     private final QualificationRepository qualificationRepository;
     private final PositionRepository positionRepository;
 
     public void sendExpirationNotifications() {
+        if (!configurationService.isNotificationEnabled(NotificationType.QUALIFICATION_EXPIRED)
+            && !configurationService.isNotificationEnabled(NotificationType.QUALIFICATION_CLOSE_TO_EXPIRED)) {
+            return;
+        }
+
         log.info("Sending user qualification expiration notifications");
         var users = userService.getDetailedUsers();
         var qualifications = qualificationRepository.findAllAsMap();
