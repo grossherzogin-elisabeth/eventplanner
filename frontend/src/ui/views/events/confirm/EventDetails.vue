c5031a9928050546adb0dc468a9bac3b45480d64
Jana <jana.rossbach@outlook.de>
2025-09-02 09:16:26 +0200

    change confirm messages for i18n

--- frontend/src/ui/views/events/confirm/EventDetails.vue
+++ frontend/src/ui/views/events/confirm/EventDetails.vue
@@ -2,26 +2,28 @@
     <table class="block sm:table">
         <tbody>
             <tr class="mb-2 block sm:table-row">
-                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">Reise:</td>
+                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">{{ $t('views.events.confirmParticipation.event') }}</td>
                 <td class="block py-1 font-bold sm:table-cell">
                     {{ props.event?.name }}
                 </td>
             </tr>
             <tr class="mb-2 block sm:table-row">
-                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">Geplante Route:</td>
+                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">{{ $t('views.events.confirmParticipation.route') }}</td>
                 <td class="block py-1 font-bold sm:table-cell">
                     {{ props.event.locations.map((l) => l.name).join(' - ') }}
                 </td>
             </tr>
             <tr class="mb-2 block sm:table-row">
-                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">Crew an Bord:</td>
+                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">
+                    {{ $t('views.events.confirmParticipation.crew-on-board') }}
+                </td>
                 <td class="block py-1 font-bold sm:table-cell">
                     {{ $d(props.event.start, DateTimeFormat.DD_MM_YYYY) }}
                     {{ $d(props.event.start, DateTimeFormat.hh_mm) }}
                 </td>
             </tr>
             <tr v-if="props.event.locations[0].etd" class="mb-2 block sm:table-row">
-                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">Reisebeginn:</td>
+                <td class="block pr-8 text-xs sm:table-cell sm:py-1 sm:text-base">{{ $t('views.events.confirmParticipation.start') }}</td>
                 <td class="block py-1 font-bold sm:table-cell">
                     {{ $d(props.event.locations[0].etd, DateTimeFormat.DD_MM_YYYY) }}
                     {{ $d(props.event.locations[0].etd, DateTimeFormat.hh_mm) }}
