3219fe1cead73213b47425220f1d377dece06388
Jana <jana.rossbach@outlook.de>
2025-09-03 15:17:43 +0200

    fir format issues

--- frontend/src/ui/locales/index.ts
+++ frontend/src/ui/locales/index.ts
@@ -1,18 +1,17 @@
-import deGeneric from './de/generic.yml';
-import deNavigation from './de/navigation.yml';
 import deComponents from './de/components.yml';
 import deDomain from './de/domain.yml';
+import deGeneric from './de/generic.yml';
+import deNavigation from './de/navigation.yml';
 import deViews from './de/views.yml';
 
 const messagesDe = {
-  ...deGeneric,
-  ...deNavigation,
-  ...deComponents,
-  ...deDomain,
-  ...deViews,
+    ...deGeneric,
+    ...deNavigation,
+    ...deComponents,
+    ...deDomain,
+    ...deViews,
 };
 
 export default {
-  de: messagesDe,
+    de: messagesDe,
 };
-
