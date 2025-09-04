b270a45e579e96f13da85f0d4a4e7e14e9abc609
Jana <jana.rossbach@outlook.de>
2025-09-04 12:53:40 +0200

    revert splitting of de.yml so the branch contains only the sceleton for the splitting

--- frontend/src/ui/locales/index.ts
+++ frontend/src/ui/locales/index.ts
@@ -1,3 +1,4 @@
+import de from './de.yml';
 import deComponents from './de/components.yml';
 import deDomain from './de/domain.yml';
 import deGeneric from './de/generic.yml';
@@ -5,6 +6,7 @@ import deNavigation from './de/navigation.yml';
 import deViews from './de/views.yml';
 
 const messagesDe = {
+    ...de,
     ...deGeneric,
     ...deNavigation,
     ...deComponents,
