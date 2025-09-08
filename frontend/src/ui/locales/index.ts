b34265fc5c8d3cc901c2befc6d86650a41a09a68
Jana <jana.rossbach@outlook.de>
2025-09-08 13:00:28 +0200

    fix format issues

--- frontend/src/ui/locales/index.ts
+++ frontend/src/ui/locales/index.ts
@@ -1,12 +1,11 @@
 import deComponents from './de/components.yml';
 import deGeneric from './de/generic.yml';
 import deNavigation from './de/navigation.yml';
-
-import deHome from './de/views/home.yml';
-import deSettings from './de/views/settings.yml';
 import deAccount from './de/views/account.yml';
 import deBasedata from './de/views/basedata.yml';
 import deEvents from './de/views/events.yml';
+import deHome from './de/views/home.yml';
+import deSettings from './de/views/settings.yml';
 import deUsers from './de/views/users.yml';
 
 const messagesDe = {
