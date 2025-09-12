ab3db0b025d71dba0505b1315a79b7699751dc56
Malte Schwitters <malte.schwitters@outlook.de>
2025-09-12 22:57:44 +0200

    remove nullability annotations in dto

--- backend/src/main/java/org/eventplanner/events/rest/users/dto/CreateUserRequest.java
+++ backend/src/main/java/org/eventplanner/events/rest/users/dto/CreateUserRequest.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 import org.eventplanner.events.domain.specs.CreateUserSpec;
 import org.springframework.lang.NonNull;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateUserRequest(
-    @NotBlank @NonNull String firstName,
-    @NotBlank @NonNull String lastName,
-    @NotBlank @NonNull String email
+    @NotBlank String firstName,
+    @NotBlank String lastName,
+    @NotBlank @Email String email
 ) implements Serializable {
 
     public @NonNull CreateUserSpec toDomain() {
