2219cb49287d77854db1401c3c302b052c9758fb
Malte Schwitters <malte.schwitters@outlook.de>
2025-09-25 22:07:29 +0200

    add integration test

---
+++ backend/src/test/java/org/eventplanner/integration/api/events/CreateExcelExportIntegrationTest.java
@@ -0,0 +1,69 @@
+package org.eventplanner.integration.api.events;
+
+import static org.eventplanner.testutil.TestUser.withAuthentication;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.eventplanner.events.adapter.jpa.events.EventJpaRepository;
+import org.eventplanner.testutil.TestUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = { "test" })
+@AutoConfigureMockMvc
+@Transactional // resets db changes after each test
+class CreateExcelExportIntegrationTest {
+    private MockMvc webMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private EventJpaRepository eventJpaRepository;
+
+    @BeforeEach
+    void setup() {
+        webMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+
+    @Test
+    void shouldRequireAuthentication() throws Exception {
+        webMvc.perform(get("/api/v1/events/7fa48570-963a-4e95-b72f-acaf70c70a24/export/imo-crew-list")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldRequireAuthorization() throws Exception {
+        webMvc.perform(get("/api/v1/events/7fa48570-963a-4e95-b72f-acaf70c70a24/export/imo-crew-list")
+                .with(withAuthentication(TestUser.TEAM_MEMBER))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void shouldGenerateExcelExport() throws Exception {
+        webMvc.perform(get("/api/v1/events/7fa48570-963a-4e95-b72f-acaf70c70a24/export/imo-crew-list")
+                .with(withAuthentication(TestUser.EVENT_PLANNER))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM));
+    }
+}
