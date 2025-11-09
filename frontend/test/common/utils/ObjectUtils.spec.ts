6492b0566256d6a36d3b0e65a00ab22b9c21e876
Malte Schwitters <malte.schwitters@outlook.de>
2025-11-09 14:55:06 +0100

    cover functions and dates

--- frontend/test/common/utils/ObjectUtils.spec.ts
+++ frontend/test/common/utils/ObjectUtils.spec.ts
@@ -39,6 +39,8 @@ describe('ObjectUtils', () => {
                 c: string;
             }[];
             diverseList: (string | number)[];
+            function?: () => string;
+            date: Date;
         }
 
         function createTestType(): TestType {
@@ -62,11 +64,14 @@ describe('ObjectUtils', () => {
                     },
                 ],
                 diverseList: [1, '2', 3, '4'],
+                function: () => 'response',
+                date: new Date('2024-07-10T09:00:00Z'),
             };
         }
 
         it('should copy all fields', async () => {
             const original = createTestType();
+            original.function = undefined;
             const copy = deepCopy(original);
             expect(copy).toEqual(original);
         });
@@ -79,8 +84,20 @@ describe('ObjectUtils', () => {
             copy.list.push('changed');
             copy.diverseList = [];
             copy.objList[0].a = 'changed';
+            copy.date = new Date();
 
             expect(copy).not.toEqual(original);
+        });
+
+        it('should wrap function calls', async () => {
+            const original = createTestType();
+            const copy = deepCopy(original);
+
+            expect(original.function).toBeDefined();
+            expect(copy.function).toBeDefined();
+            if (original.function && copy.function) {
+                expect(copy.function()).toEqual(original.function());
+            }
         });
     });
 });
