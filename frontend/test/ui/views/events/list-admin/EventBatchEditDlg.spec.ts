20d3942f74477e6ae7e1b89cc700204420c08588
Malte Schwitters <malte.schwitters@outlook.de>
2025-12-27 17:54:22 +0100

    fix teleport errors

--- frontend/test/ui/views/events/list-admin/EventBatchEditDlg.spec.ts
+++ frontend/test/ui/views/events/list-admin/EventBatchEditDlg.spec.ts
@@ -2,6 +2,7 @@ import { nextTick } from 'vue';
 import type { MockInstance } from 'vitest';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import type { VueWrapper } from '@vue/test-utils';
+import { flushPromises } from '@vue/test-utils';
 import { mount } from '@vue/test-utils';
 import { HttpResponse, http } from 'msw';
 import { useEventAdministrationUseCase } from '@/application';
@@ -13,7 +14,6 @@ import { EventState } from '@/domain';
 import type { Dialog } from '@/ui/components/common';
 import EventBatchEditDlg from '@/ui/views/events/list-admin/EventBatchEditDlg.vue';
 import { mockEvent, server } from '~/mocks';
-import { find, findAll } from '~/utils';
 
 describe('EventBatchEditDlg.vue', () => {
     let testee: VueWrapper;
@@ -22,7 +22,7 @@ describe('EventBatchEditDlg.vue', () => {
 
     beforeEach(async () => {
         result = undefined;
-        testee = mount(EventBatchEditDlg);
+        testee = mount(EventBatchEditDlg, { global: { stubs: { teleport: true } } });
         updateFunc = vi.spyOn(useEventAdministrationUseCase(), 'updateEvents');
         const eventA = mockEvent({ key: 'a' });
         const eventB = mockEvent({ key: 'b' });
@@ -32,41 +32,46 @@ describe('EventBatchEditDlg.vue', () => {
     });
 
     it('should open dialog', async () => {
-        const dialog = find('[data-test-id="event-batch-edit-dialog"]');
+        const dialog = testee.find('[data-test-id="event-batch-edit-dialog"]');
         expect(dialog.isVisible()).toBe(true);
+        await flushPromises();
     });
 
     it('should close dialog on cancel', async () => {
         await cancel();
         expect(updateFunc).not.toHaveBeenCalled();
         expect(result).toBe(false);
+        await flushPromises();
     });
 
     it('should close dialog on submit without input', async () => {
         await submit();
         expect(updateFunc).not.toHaveBeenCalled();
         expect(result).toBe(false);
+        await flushPromises();
     });
 
     it.each([EventState.Draft, EventState.OpenForSignup, EventState.Planned, EventState.Canceled])(
         'should patch only event status to $0',
         async (state) => {
-            const input = find('[data-test-id="input-event-status"] input');
+            const input = testee.find('[data-test-id="input-event-status"] input');
             await input.trigger('click');
             await selectOption(testee.vm.$t(`generic.event-state.${state}`));
 
             await submit();
             expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { state });
             expect(result).toBe(true);
+            await flushPromises();
         }
     );
 
     it('should patch only event name', async () => {
-        const input = find('[data-test-id="input-event-name"] input');
+        const input = testee.find('[data-test-id="input-event-name"] input');
         await input.setValue('updated');
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { name: 'updated' });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     it.each([
@@ -77,63 +82,69 @@ describe('EventBatchEditDlg.vue', () => {
         EventType.TrainingEvent,
         EventType.Other,
     ])('should patch only event type to $0', async (type) => {
-        const input = find('[data-test-id="input-event-type"] input');
+        const input = testee.find('[data-test-id="input-event-type"] input');
         await input.trigger('click');
         await selectOption(testee.vm.$t(`generic.event-type.${type}`));
 
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { type });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     it.each([EventSignupType.Assignment, EventSignupType.Open])('should patch only event signup type to $0', async (signupType) => {
-        const input = find('[data-test-id="input-event-signup-type"] input');
+        const input = testee.find('[data-test-id="input-event-signup-type"] input');
         await input.trigger('click');
         await selectOption(testee.vm.$t(`generic.event-signup-type.${signupType}`));
 
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { signupType });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     it('should patch only event slots', async () => {
-        const input = find('[data-test-id="input-event-slots"] input');
+        const input = testee.find('[data-test-id="input-event-slots"] input');
         await input.trigger('click');
         await selectOption(1);
 
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { slots: mockEvent().slots });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     it('should show warning when changing slots', async () => {
-        const input = find('[data-test-id="input-event-slots"] input');
+        const input = testee.find('[data-test-id="input-event-slots"] input');
         await input.trigger('click');
         await selectOption(1);
-        const warning = find('[data-test-id="warning-slots-overwrite"]');
+        const warning = testee.find('[data-test-id="warning-slots-overwrite"]');
         expect(warning.isVisible()).toBe(true);
+        await flushPromises();
     });
 
     it('should patch only event description', async () => {
-        const input = find('[data-test-id="input-event-description"] textarea');
+        const input = testee.find('[data-test-id="input-event-description"] textarea');
         await input.setValue('updated');
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { description: 'updated' });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     it('should patch event name and description', async () => {
-        const name = find('[data-test-id="input-event-name"] input');
+        const name = testee.find('[data-test-id="input-event-name"] input');
         await name.setValue('updated');
-        const description = find('[data-test-id="input-event-description"] textarea');
+        const description = testee.find('[data-test-id="input-event-description"] textarea');
         await description.setValue('updated');
         await submit();
         expect(updateFunc).toHaveBeenCalledExactlyOnceWith(['a', 'b'], { name: 'updated', description: 'updated' });
         expect(result).toBe(true);
+        await flushPromises();
     });
 
     async function selectOption(option: string | number): Promise<void> {
-        const options = findAll('.input-dropdown li');
+        const options = testee.findAll('.input-dropdown li');
         if (typeof option === 'string') {
             await options.find((it) => it.text() === option)?.trigger('click');
         } else {
@@ -142,19 +153,18 @@ describe('EventBatchEditDlg.vue', () => {
     }
 
     async function cancel(): Promise<void> {
-        await find('[data-test-id="button-cancel"]').trigger('click');
+        await testee.find('[data-test-id="button-cancel"]').trigger('click');
         await nextTick(); // wait for result to be passed to variable
         await wait(1); // wait 1ms to make sure all network requests made after opening
     }
 
     async function submit(): Promise<void> {
-        await find('[data-test-id="button-submit"]').trigger('click');
+        await testee.find('[data-test-id="button-submit"]').trigger('click');
         await nextTick(); // wait for result to be passed to variable
         await wait(1); // wait 1ms to make sure all network requests made after opening
     }
 
     async function open(events: Event[]): Promise<void> {
-        // const cmp = testee.findComponent('[data-test-id="event-batch-edit-dialog"]');
         const dialog = testee.getCurrentComponent().exposed as Dialog<Event[], boolean>;
         // don't await here, because the promise will only be resolved when the dialog is closed
         dialog.open(events).then((r) => (result = r));
