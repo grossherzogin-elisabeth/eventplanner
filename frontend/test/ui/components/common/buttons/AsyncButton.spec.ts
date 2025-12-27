342605fade3d57cedfd91785cf23aefb1bdb6630
Malte Schwitters <malte.schwitters@outlook.de>
2025-12-27 22:01:07 +0100

    fix build

--- frontend/test/ui/components/common/buttons/AsyncButton.spec.ts
+++ frontend/test/ui/components/common/buttons/AsyncButton.spec.ts
@@ -1,4 +1,3 @@
-import { nextTick } from 'vue';
 import { beforeEach, describe, expect, it } from 'vitest';
 import type { VueWrapper } from '@vue/test-utils';
 import { mount } from '@vue/test-utils';
