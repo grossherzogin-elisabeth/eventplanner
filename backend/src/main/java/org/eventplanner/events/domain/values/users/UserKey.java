38315b744afac6a5458dfc43fc8ccf4240d1ad91
Jana <jana.rossbach@outlook.de>
2026-01-19 14:21:21 +0100

    avoid logging user controlled data

--- backend/src/main/java/org/eventplanner/events/domain/values/users/UserKey.java
+++ backend/src/main/java/org/eventplanner/events/domain/values/users/UserKey.java
@@ -1,9 +1,6 @@
 package org.eventplanner.events.domain.values.users;
 
 import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 import org.springframework.lang.NonNull;
@@ -17,45 +14,17 @@ public record UserKey(
     }
 
     public UserKey(@Nullable String value) {
-        if (value != null && !value.isBlank()) {
+        if (value == null) {
+            this.value = UUID.randomUUID().toString();
+        } else if (value.matches("[a-z0-9\\-]+")) {
             this.value = value;
         } else {
-            this.value = UUID.randomUUID().toString();
-        }
-    }
-
-    public static @NonNull UserKey fromName(@NonNull String name) {
-        var normalizedName = name
-            .replace("mit Ü", "")
-            .replace("u. V.", "")
-            .replace("?", "")
-            .toLowerCase()
-            .replaceAll("\\s", "") // remove whitespace characters
-            .replaceAll("\\(.*\\)", "") // remove everything in brackets e.g. (this)
-            .replaceAll("[^a-zA-ZöäüÖÄÜß\\-., ]", ""); // remove all non a-z characters
-        if (normalizedName.contains(",")) {
-            var parts = normalizedName.split(",");
-            normalizedName = parts[1] + parts[0];
-        }
-
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(normalizedName.getBytes(StandardCharsets.UTF_8));
-            StringBuilder hexString = new StringBuilder(2 * hash.length);
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-            return new UserKey(hexString.substring(0, 16));
-        } catch (NoSuchAlgorithmException _) {
-            throw new RuntimeException();
+            throw new IllegalArgumentException("UserKey value must match [a-z0-9\\-]+");
         }
     }
 
     @Override
+    @NonNull
     public String toString() {
         return value;
     }
