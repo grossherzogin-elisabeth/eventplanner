c7ac1e173cf846ac3fc8d71bd4f153de354eedf8
Jana <jana.rossbach@outlook.de>
2026-01-19 10:00:09 +0100

    fix some minor sonar issues

--- backend/src/test/java/org/eventplanner/events/application/services/EncryptionServiceTest.java
+++ backend/src/test/java/org/eventplanner/events/application/services/EncryptionServiceTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @SuppressWarnings("DataFlowIssue")
-public class EncryptionServiceTest {
+class EncryptionServiceTest {
 
     private EncryptionService testee;
 
