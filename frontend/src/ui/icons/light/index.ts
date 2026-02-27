d839739fe3c2438a2142de047bc3070ade1b15e6
Malte Schwitters <malte.schwitters@outlook.de>
2026-02-27 21:09:00 +0100

    delete unused files

--- frontend/src/ui/icons/light/index.ts
+++
@@ -1,5 +0,0 @@
-import IconArrowDown from './arrow-down-1.svg?component';
-import IconDate from './calendar-date.svg?component';
-import IconSearch from './search.svg?component';
-
-export { IconArrowDown, IconDate, IconSearch };
