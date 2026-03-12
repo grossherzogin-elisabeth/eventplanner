50011b965ee0d81cbcc3fb7b7b10e3befd8ce14c
Malte Schwitters <malte.schwitters@outlook.de>
2026-03-12 21:02:46 +0100

    add more tests for event list view

--- frontend/test/utils/permissions.ts
+++ frontend/test/utils/permissions.ts
@@ -1,10 +1,11 @@
 import { useAuthService } from '@/application';
-import type { Permission } from '@/domain';
+import type { Permission, SignedInUser } from '@/domain';
 import { mockSignedInUser } from '~/mocks';
 
-export function setupUserPermissions(permissions: Permission[]): void {
+export function setupUserPermissions(permissions: Permission[]): SignedInUser {
     const authService = useAuthService();
     const signedInUser = mockSignedInUser();
     signedInUser.permissions.push(...permissions);
     authService.setSignedInUser(signedInUser);
+    return signedInUser;
 }
