1b85f959a4ee07b9cafb65c3cc004e210c8a2058
Malte Schwitters <malte.schwitters@outlook.de>
2026-05-29 13:50:03 +0200

    Migrate test db to setup scripts (#653)
    * add db setup scripts for test data
    
    * use flyway migration to create testdata

---
+++ backend/src/test/resources/db/testdata/V101__testdata_settings.sql
@@ -0,0 +1,45 @@
+INSERT INTO settings (key, value)
+VALUES ('email.from', 'from@email.com');
+
+INSERT INTO settings (key, value)
+VALUES ('email.fromDisplayName', 'From Displayname');
+
+INSERT INTO settings (key, value)
+VALUES ('email.replyTo', 'reply-to@email.com');
+
+INSERT INTO settings (key, value)
+VALUES ('email.replyToDisplayName', 'Reply To Displayname');
+
+INSERT INTO settings (key, value)
+VALUES ('email.host', 'smtp.office365.com');
+
+INSERT INTO settings (key, value)
+VALUES ('email.port', '587');
+
+INSERT INTO settings (key, value)
+VALUES ('email.enableSSL', 'false');
+
+INSERT INTO settings (key, value)
+VALUES ('email.enableStartTLS', 'true');
+
+INSERT INTO settings (key, value)
+VALUES ('email.username', 'username');
+
+INSERT INTO settings (key, value)
+VALUES ('ui.menuTitle', 'Test');
+
+INSERT INTO settings (key, value)
+VALUES ('ui.tabTitle', 'Test');
+
+INSERT INTO settings (key, value)
+VALUES ('ui.technicalSupportEmail', 'admin@email.com');
+
+INSERT INTO settings (key, value)
+VALUES ('ui.supportEmail', 'support@email.com');
+
+INSERT INTO settings (key, value)
+VALUES ('notifications.teamsWebhookUrl',
+        'TAxE75wnetu4nE15yAuWHk+I3zVUj+9sJTZ8h6paw9KTEhgOEP4q8zjcY8E6hrJC+Zd33Ev3HT0428piDL0pfuOU+aeV4q07vk5VK/rMVHo=');
+
+INSERT INTO settings (key, value)
+VALUES ('email.password', 'q6ZeEQlo4nZbElDDRU5gY142yD3n+9+B1/UCtVhGya0=');
---
+++ backend/src/test/resources/db/testdata/V103__testdata_qualifications.sql
@@ -0,0 +1,39 @@
+INSERT INTO qualifications (key, name, icon, description, expires, grants_position)
+VALUES ('7e819522-6103-491c-8ef0-cbc0c2c208dd',
+        'Captain',
+        'fa-anchor',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.',
+        1,
+        '4b2ff7f9-9ec9-44d0-8de3-57bbefed2545');
+
+INSERT INTO qualifications (key, name, icon, description, expires, grants_position)
+VALUES ('659fccf8-2bfc-4123-b7cd-98be4b8e4f8d',
+        'Engineer',
+        'fa-screwdriver-wrench',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.',
+        0,
+        '08c609d2-26c9-4848-b17b-8feac2e2a91f');
+
+INSERT INTO qualifications (key, name, icon, description, expires, grants_position)
+VALUES ('d470b84e-4da8-40cd-87bd-05c0b0421927',
+        'Mate',
+        'fa-id-card',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+        0,
+        '3ea10356-8f2f-49df-abc8-b9ecd1a6934e');
+
+INSERT INTO qualifications (key, name, icon, description, expires, grants_position)
+VALUES ('9e4f0b6c-f7e6-480e-8b16-28e50e25f9d3',
+        'Trainer',
+        'fa-bullhorn',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
+        0,
+        '');
+
+INSERT INTO qualifications (key, name, icon, description, expires, grants_position)
+VALUES ('225f84e5-ebc1-4e76-8ada-98df9f8bf4ad',
+        'Deck',
+        'fa-helmet-safety',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
+        0,
+        '24dd5aef-e819-4d9d-8b19-d9d592c4ff8f');
