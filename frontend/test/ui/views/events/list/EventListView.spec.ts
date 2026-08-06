4b684f47699154fbe6a8c4a2e3de3591db3f0866
Malte Schwitters <malte.schwitters@outlook.de>
2026-08-07 00:07:36 +0200

    fix tests

--- frontend/test/ui/views/events/list/EventListView.spec.ts
+++ frontend/test/ui/views/events/list/EventListView.spec.ts
@@ -153,8 +153,8 @@ describe('EventListView.vue', () => {
     it('should render export actions in row context menu', async () => {
         await loading();
         const menu = await openTableContextMenu(testee, 0);
+        await expect.poll(() => menu.findAll('[data-test-id="action-export"]')).toHaveLength(2);
         const exports = menu.findAll('[data-test-id="action-export"]');
-        expect(exports).toHaveLength(2);
         expect(exports[0].text()).toContain('some template');
         expect(exports[1].text()).toContain('some other template');
     });
