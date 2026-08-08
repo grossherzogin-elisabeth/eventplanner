831e9fc597387a6331c0e808ed36ddc944ef6fe3
Malte Schwitters <malte.schwitters@outlook.de>
2026-08-08 14:38:00 +0200

    add config composable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -48,13 +48,13 @@ const chunks = {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-    const host = process.env.VITE_HOST;
-    const port = Number.parseInt(process.env.VITE_PORT as string, 10);
-    const serviceUrl = process.env.VITE_SERVICE_URL;
+    const host = process.env.VITE_DEV_SERVER_HOST;
+    const port = Number.parseInt(process.env.VITE_DEV_SERVER_PORT as string, 10);
+    const serviceUrl = process.env.VITE_DEV_SERVER_SERVICE_URL;
 
-    if (!host) throw new Error('env variable VITE_HOST is not set');
-    if (!port) throw new Error('env variable VITE_PORT is not set');
-    if (!serviceUrl) throw new Error('env variable VITE_SERVICE_URL is not set');
+    if (!host) throw new Error('env variable VITE_DEV_SERVER_HOST is not set');
+    if (!port) throw new Error('env variable VITE_DEV_SERVER_PORT is not set');
+    if (!serviceUrl) throw new Error('env variable VITE_DEV_SERVER_SERVICE_URL is not set');
 
     return {
         plugins: [vue(), svgLoader(), pwa, ViteYaml()],
