a77f8f572af6eff73df038f7499e808c66ce2149
Malte Schwitters <malte.schwitters@outlook.de>
2026-08-16 21:41:09 +0200

    improve i18n key detection for testing

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import viteConfig from './vite.config.ts';
 
 export default mergeConfig(
     viteConfig({ mode: 'test', command: 'serve' }),
