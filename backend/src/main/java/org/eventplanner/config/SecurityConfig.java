35f5bd41994230f1271c75882ae18d75c9be4060
Malte Schwitters <malte.schwitters@outlook.de>
2026-08-29 21:59:34 +0200

    revert csrf flag removal

--- backend/src/main/java/org/eventplanner/config/SecurityConfig.java
+++ backend/src/main/java/org/eventplanner/config/SecurityConfig.java
@@ -5,12 +5,16 @@ import org.eventplanner.auth.ConvertToSignedInUserAuthenticationFilter;
 import org.eventplanner.auth.OAuthClientConfig;
 import org.eventplanner.auth.RefreshSignedInUserAuthenticationFilter;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
@@ -19,14 +23,12 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final OAuthClientConfig oAuthClientConfig;
@@ -34,12 +36,35 @@ public class SecurityConfig {
     private final ConvertToSignedInUserAuthenticationFilter convertToSignedInUserAuthenticationFilter;
     private final RefreshSignedInUserAuthenticationFilter refreshSignedInUserAuthenticationFilter;
     private final LogRequestsFilter logRequestsFilter;
+    private final boolean enableCSRF;
+
+    public SecurityConfig(
+        @NonNull @Autowired final OAuthClientConfig oAuthClientConfig,
+        @NonNull @Autowired final ConvertToAccessKeyAuthenticationFilter convertToAccessKeyAuthenticationFilter,
+        @NonNull @Autowired final ConvertToSignedInUserAuthenticationFilter convertToSignedInUserAuthenticationFilter,
+        @NonNull @Autowired final RefreshSignedInUserAuthenticationFilter refreshSignedInUserAuthenticationFilter,
+        @NonNull @Autowired final LogRequestsFilter logRequestsFilter,
+        @Nullable @Value("${auth.csrf.enabled}") String enableCSRF
+    ) {
+        this.oAuthClientConfig = oAuthClientConfig;
+        this.convertToAccessKeyAuthenticationFilter = convertToAccessKeyAuthenticationFilter;
+        this.convertToSignedInUserAuthenticationFilter = convertToSignedInUserAuthenticationFilter;
+        this.refreshSignedInUserAuthenticationFilter = refreshSignedInUserAuthenticationFilter;
+        this.logRequestsFilter = logRequestsFilter;
+        this.enableCSRF = "true".equals(enableCSRF);
+    }
 
     @Bean
     public @NonNull SecurityFilterChain securityConfigCustomizer(@NonNull HttpSecurity http) {
-        http.csrf(csrf -> csrf
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .spa());
+        if (enableCSRF) {
+            http.csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .spa());
+        } else {
+            // disable CSRF, useful for testing
+            log.warn("CSRF protection is disabled");
+            http.csrf(AbstractHttpConfigurer::disable);
+        }
 
         // By default, Spring redirects an unauthorized user to the login page. In this case we want to return a 401
         // error and let the frontend handle the login flow.
